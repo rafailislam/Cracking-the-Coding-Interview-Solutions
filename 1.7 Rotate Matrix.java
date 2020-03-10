2caa98725218c81a0412fc83ff3901ca6f89e9aa
Rafail Islam <rafail.islam.bd@gmail.com>
2020-03-10 12:17:33 -0500

    Update 1.7 Rotate Matrix.java
    This is in place solution same as book solution

--- 1.7 Rotate Matrix.java
+++ 1.7 Rotate Matrix.java
@@ -1,4 +1,6 @@
+/*
 // not in place solution
+
 public class Main{
     public static int [][] rotation( int [][] arr, int n){
         int [][] narray = new int [n][n];
@@ -45,4 +47,47 @@ public class Main{
         }
         
      }
+}
+*/
+public class Main
+{
+    public static boolean rotation(int [][] arr){
+        if ( arr.length == 0 || arr.length != arr[0].length )
+            return false;
+        int n = arr.length;
+        
+        for (int layer = 0; layer<n/2; layer++)
+        {
+            int first = layer;
+            int last = n-1-layer;
+            for(int i = first; i< last; i++){
+                int offset = i-first;
+                int temp = arr[first][i];
+                
+                arr[first][i] = arr[last-offset][first];
+                arr[last-offset][first] = arr[last][last - offset];
+                arr[last][last - offset] = arr[i][last];
+                
+                arr[i][last] = temp;
+            }
+        }
+        for(int i=0;i<arr.length; i++){
+            for(int j=0;j<arr.length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+	    int n = 6;
+	    int c=1;
+	    int arr[][] = new int[n][n];
+	    for(int i=0;i<n; i++){
+            for(int j=0;j<n; j++){
+                arr[i][j] = c++;
+            }
+        }
+		System.out.println(rotation(arr) );
+	}
 }
